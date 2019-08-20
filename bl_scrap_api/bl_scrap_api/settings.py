8d542762b3a51cfd8f521daa82cdd6a989a7c870
jefri <sosmed.jefrihutama@gmail.com>
2019-08-20 23:18:14 +0700

    scraps API

--- bl_scrap_api/bl_scrap_api/settings.py
+++ bl_scrap_api/bl_scrap_api/settings.py
@@ -84,21 +84,10 @@ WSGI_APPLICATION = 'bl_scrap_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-     'HOST': 'localhost',
-     'PORT': '5432',
-     'NAME': 'bukalapak',
-     'USER': 'postgres',
-     'PASSWORD': 'admin'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
